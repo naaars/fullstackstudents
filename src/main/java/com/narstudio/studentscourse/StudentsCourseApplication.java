5cc34c0ba0c0c8e3bc1ee55acdb95105c26a7266
Inigo Moreno <inigomorenotato@gmail.com>
2024-01-11 22:28:48 +0100

    Bootstrap Application with Spring Innitializr
    Bootstrap Application with Spring Innitializr
    
    Bootstrap Application with Spring Innitializr

---
+++ src/main/java/com/narstudio/studentscourse/StudentsCourseApplication.java
@@ -0,0 +1,13 @@
+package com.narstudio.studentscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsCourseApplication.class, args);
+	}
+
+}
