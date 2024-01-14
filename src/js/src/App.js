8c428ba4950e6ca454c741984b96d1db49625bcf
Inigo Moreno <inigomorenotato@gmail.com>
2024-01-14 17:57:48 +0100

    Added antd and table component

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import Container from "./container.js";
 import "./App.css";
 import { getStudents } from "./client.js";
+import { Table } from "antd";
 
 class App extends Component {
     state = {
@@ -22,17 +24,39 @@ class App extends Component {
     render() {
         const { students } = this.state;
         if (students && students.length) {
-            return students.map((student, index) => {
-                return (
-                    <div key={index}>
-                        <h2>{student.studentId}</h2>
-                        <p>{student.firstName}</p>
-                        <p>{student.lastName}</p>
-                        <p>{student.gender}</p>
-                        <p>{student.email}</p>
-                    </div>
-                );
-            });
+            const columns = [
+                {
+                    title: "Id",
+                    dataIndex: "studentId",
+                    key: "studentId"
+                },
+                {
+                    title: "First Name",
+                    dataIndex: "firstName",
+                    key: "firstName"
+                },
+                {
+                    title: "Last Name",
+                    dataIndex: "lastName",
+                    key: "lastName"
+                },
+                {
+                    title: "Email",
+                    dataIndex: "email",
+                    key: "email"
+                },
+                {
+                    title: "Gender",
+                    dataIndex: "gender",
+                    key: "gender"
+                }
+            ];
+
+            return (
+                <Container>
+                    <Table dataSource={students} columns={columns} />
+                </Container>
+            );
         }
         return <h1>No students found</h1>;
     }
